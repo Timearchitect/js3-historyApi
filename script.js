f5dc816e0f545e06502cfd88d3e29123818b6cc7
Alrik He <alrik_he@hotmail.com>
2024-09-16 15:23:50 +0200

    fixed

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ window.onload = fetchHtml('home')
   });
  */
 
-
 window.addEventListener('popstate', (event) => {
     /*     alert(
       `location: ${document.location}, state: ${JSON.stringify(event.state)}`,
@@ -26,16 +25,6 @@ window.addEventListener('popstate', (event) => {
         default:
             fetchHtml('home')
     }
-
-    if (1 + 1 == 2) 
-        console.log(1)
-    else if (true) 
-        console.log(1)
-    else if (true) 
-        console.log(1)
-     else 
-        console.log(1)
-    
 })
 
 home.addEventListener('click', (e) => {
